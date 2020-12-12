0afdeec79d5d856043b155df2b830616ba47ae92
Surya Jitendra Kumar Nallamalli <suryajkn@a483e7e3c79f.ant.amazon.com>
2020-12-13 00:33:47 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,18 +1,52 @@
-# Welcome to your CDK Java project!
+# Provision Amazon Managed Blockchain Client EC2 Instance using CDK and Java Programming Language
 
-This is a blank project for Java development with CDK.
+## Introduction
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+This Sample provides Java code to provision Amazon Managed Blockchain Client EC2 Instance using AWS Cloud Development Kit(CDK).
 
-It is a [Maven](https://maven.apache.org/) based project, so you can open this project with any Maven compatible Java IDE to build and run tests.
+Know more details about AWS CDK here https://aws.amazon.com/cdk/
 
-## Useful commands
+Know more details about Amazon Managed Blockchain here https://aws.amazon.com/managed-blockchain
 
- * `mvn package`     compile and run tests
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
+## Prerequisites
 
-Enjoy!
+* Java 8 (v1.8) or later
+* Apache Maven 3.5 or later
+* CDK 1.69 
+* AWS Command Line Interface V2
+
+## How to use this Sample
+
+Before using code from this sample make sure following are provisioned/created.
+
+* **Amazon Managed Blockchain Network** You may refer this [Sample](https://github.com/aws-samples/amazon-managed-blockchain-network-provision-with-cdk-java) to provision using CDK 
+* **VPC**
+* **Public Subnet**
+* **Security Group**
+* **Rules to allow Inbound and Outbound traffic between members of Security Group**
+* **Interface VPC Endpoint to connect to Amazon Managed Blockchain** You may refer this [URL](https://docs.aws.amazon.com/managed-blockchain/latest/managementguide/get-started-create-endpoint.html)
+* **IAM role for Amazon EC2 instance** You may refer this [URL](https://docs.aws.amazon.com/managed-blockchain/latest/managementguide/security_iam_hyperledger_ec2_client.html)
+* **SSH key pair for Amazon EC2 instance**
+
+Provide below values in AmbNetworkClientApp.java file.
+
+* **`<ACCOUNT ID>`**
+* **`<REGION>`**
+
+Provide below values in AmbNetworkClientStack.java file.
+
+* **`<VPD ID>`**
+* **`<SECURITY GROUP ID>`**
+* **`<EC2 INSTANCE TYPE>`**
+* **`<KEY PAIR NAME>`**
+* **`<INSTANCE IAM ROLE ARN>`**
+* **`<HYPERLEDGER FABRIC VERSION>`**
+* **`<MEMBER ID>`**
+* **`<PEER END POINT>`**
+* **`<ORDERING SERVICE END POINT>`**
+* **`<CERTIFICATE AUTHORITY(CA) END POINT>`**
+
+After providing above values, execute the following commands in terminal from the root directory.
+
+* `cdk synth` this command generates the CloudFormation template under cdk.out folder. Validate the template.
+* `cdk deploy` this command deploys CloudFormation stack
--- src/main/java/com/myorg/AmbNetworkClientStack.java
+++ src/main/java/com/myorg/AmbNetworkClientStack.java
@@ -1,37 +1,42 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.myorg;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import org.jetbrains.annotations.NotNull;
-
-import software.amazon.awscdk.core.CfnOutput;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.cxapi.VpcSubnet;
 import software.amazon.awscdk.services.ec2.AmazonLinuxImage;
-import software.amazon.awscdk.services.ec2.ExecuteFileOptions;
-import software.amazon.awscdk.services.ec2.IPublicSubnet;
 import software.amazon.awscdk.services.ec2.ISecurityGroup;
-import software.amazon.awscdk.services.ec2.ISubnet;
 import software.amazon.awscdk.services.ec2.IVpc;
 import software.amazon.awscdk.services.ec2.Instance;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ec2.InterfaceVpcEndpoint;
-import software.amazon.awscdk.services.ec2.Port;
-import software.amazon.awscdk.services.ec2.PublicSubnet;
 import software.amazon.awscdk.services.ec2.SecurityGroup;
-import software.amazon.awscdk.services.ec2.SelectedSubnets;
-import software.amazon.awscdk.services.ec2.Subnet;
-import software.amazon.awscdk.services.ec2.SubnetConfiguration;
 import software.amazon.awscdk.services.ec2.SubnetSelection;
 import software.amazon.awscdk.services.ec2.SubnetType;
 import software.amazon.awscdk.services.ec2.UserData;
 import software.amazon.awscdk.services.ec2.Vpc;
-import software.amazon.awscdk.services.ec2.VpcAttributes;
-import software.amazon.awscdk.services.ec2.VpcEndpointService;
 import software.amazon.awscdk.services.ec2.VpcLookupOptions;
 import software.amazon.awscdk.services.iam.IRole;
 import software.amazon.awscdk.services.iam.Role;
@@ -43,17 +48,17 @@ public class AmbNetworkClientStack extends Stack {
 
         public AmbNetworkClientStack(final Construct scope, final String id, final StackProps props) {
                 super(scope, id, props);
-                
-                final String vpcId = "vpc-6c8fd116";
-                final String securityGroupId = "sg-a7512bf2";
-                final String instanceType = "t3.small";
-                final String keyPairName = "amb-client";
-                final String instaceRoleARN = "arn:aws:iam::036842120113:role/ServiceRoleForHyperledgerFabricClient";
-                final String frameworkVersion = "1.4";
-                final String memberID = "m-KGBFM73CABATRKJZT7XN7S4UJY";
-                final String peerNodeEndpoint = "nd-w7fqnwo2ajgklghrjhwz3j2ome.m-kgbfm73cabatrkjzt7xn7s4ujy.n-yz36mjouonbwnjqbao5m7gu624.managedblockchain.us-east-1.amazonaws.com:30003"; 
-                final String orderingServiceEndpoint = "orderer.n-yz36mjouonbwnjqbao5m7gu624.managedblockchain.us-east-1.amazonaws.com:30001"; 
-                final String fabricCAEndpoint = "ca.m-kgbfm73cabatrkjzt7xn7s4ujy.n-yz36mjouonbwnjqbao5m7gu624.managedblockchain.us-east-1.amazonaws.com:30002";
+
+                final String vpcId = "<VPD ID>";
+                final String securityGroupId = "<SECURITY GROUP ID>";
+                final String instanceType = "<EC2 INSTANCE TYPE>"; // e.g. t3.small
+                final String keyPairName = "<KEY PAIR NAME>"; 
+                final String instaceRoleARN = "<INSTANCE IAM ROLE ARN>";
+                final String frameworkVersion = "<HYPERLEDGER FABRIC VERSION>";//e.g. 1.4
+                final String memberID = "<MEMBER ID>";
+                final String peerNodeEndpoint = "<PEER END POINT>";
+                final String orderingServiceEndpoint = "<ORDERING SERVICE END POINT>";
+                final String fabricCAEndpoint = "<CERTIFICATE AUTHORITY(CA) END POINT>";
 
                 IVpc clientVPC = Vpc.fromLookup(this, "AMBClientVPC", VpcLookupOptions.builder().vpcId(vpcId).build());
 
@@ -62,12 +67,14 @@ public class AmbNetworkClientStack extends Stack {
 
                 IRole instaceRole = Role.fromRoleArn(this, "InstanceRole", instaceRoleARN);
 
-                String dockerComposeVersion = ""; 
+                String dockerComposeVersion = "";
                 String goVersion = "";
                 String fabricToolsVersion = "";
                 String fabricCAVersion = "";
                 String fabricSamplesBranch = "";
-                String tlsCertURL = String.format("https://s3.%s.amazonaws.com/%s.managedblockchain/etc/managedblockchain-tls-chain.pem", this.getRegion(), this.getRegion());
+                String tlsCertURL = String.format(
+                                "https://s3.%s.amazonaws.com/%s.managedblockchain/etc/managedblockchain-tls-chain.pem",
+                                this.getRegion(), this.getRegion());
 
                 if (frameworkVersion.equals("1.2")) {
                         dockerComposeVersion = "1.20.0";
@@ -83,13 +90,25 @@ public class AmbNetworkClientStack extends Stack {
                         fabricSamplesBranch = "release-1.4";
                 }
 
+                Path path = Paths.get(System.getProperty("user.dir") + "/resources/userdata.sh");
+                String userDataScript = "";
+                try {
+                        userDataScript = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                userDataScript = userDataScript.replace("${DOCKER_COMPOSE_VERSION}", dockerComposeVersion);
+                userDataScript = userDataScript.replace("${GO_VERSION}", goVersion);
+                userDataScript = userDataScript.replace("${FABRIC_CA_VERSION}", fabricCAVersion);
+                userDataScript = userDataScript.replace("${TLS_CERT_URL}", tlsCertURL);
+                userDataScript = userDataScript.replace("${FABRIC_TOOLS_VERSION}", fabricToolsVersion);
+                userDataScript = userDataScript.replace("${FABRIC_SAMPLES_BRANCH}", fabricSamplesBranch);
+                userDataScript = userDataScript.replace("${MEMBER_ID}", memberID);
+                userDataScript = userDataScript.replace("${PEER_NODE_ENDPOINT}", peerNodeEndpoint);
+                userDataScript = userDataScript.replace("${ORDERING_SERVICE_ENDPOINT}", orderingServiceEndpoint);
+                userDataScript = userDataScript.replace("${FABRIC_CA_ENDPOINT}", fabricCAEndpoint);
 
-                String userDataArguments = String.format("%s %s %s %s %s %s %s %s %s %s", dockerComposeVersion,
-                                goVersion, fabricCAVersion, tlsCertURL, fabricToolsVersion, fabricSamplesBranch,
-                                memberID, peerNodeEndpoint, orderingServiceEndpoint, fabricCAEndpoint);
-                UserData userData = UserData.forLinux();
-                userData.addExecuteFileCommand(ExecuteFileOptions.builder().filePath("/resources/userdata.sh")
-                                .arguments(userDataArguments).build());
+                UserData userData = UserData.custom(userDataScript);
 
                 Instance clientInstance = Instance.Builder.create(this, "AMBClientInstance")
                                 .instanceType(new InstanceType(instanceType)).machineImage(new AmazonLinuxImage())
